910fc09dc723bfd233fb3441715a77dfeed451c8
Dani <danikools@icloud.com>
2021-01-11 22:30:19 +0100

    Home controller and home page.
    Home controller works and shows the /home page in the show function. Created a simple header with a h1 and some a tags in a li and gave it some style with CSS

---
+++ version1.2/resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/style.css') }}" />
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="navbar">
+            <h1>Dani</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Over mij</a></li>
+                    <li><a href="">Projecten</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</body>
+</html>
